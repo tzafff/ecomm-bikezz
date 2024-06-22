f0fd5c07776dc770a49d2930d51e0b1710839b89
tzafff <christzaf99@gmail.com>
2024-06-22 16:52:00 +0300

    Css, tailwind,shadcn  Set Up,

---
+++ app/page.jsx
@@ -0,0 +1,8 @@
+import PopularBikes from "@/components/PopularBikes";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main><PopularBikes /></main>
+  );
+}
---
+++ components/PopularBikes.jsx
@@ -0,0 +1,37 @@
+import { client } from "@/sanity/lib/client";
+import Link from "next/link";
+
+const getData = async() => {
+    const query = `*[_type == 'product' && references(*[_type == 'category' && name == 'popular']._id, categories) ]{
+  _id,
+    name,
+    description,
+    images,
+    price,
+    price_id,
+    "slug": slug.current,
+    "categories": categories[]->{
+    name
+    }
+}`;
+const data = await client.fetch(query);
+
+return data;
+}
+
+const PopularBikes = async () => {
+    //const bikes = await getData();
+    //console.log(bikes);
+  return <section className="py-24">
+    <div className="container mx-auto">
+      <h2 className="text-center">Most Popular Bikes</h2>
+      <p className="text-center mb-[30px]">The Wolrd's Premium Brands In One Destination</p>
+      <div>Carousel</div>
+      <Link href="/our-bikes">
+        <button className="btn btn-accent mx-auto ">See all bikes</button>
+      </Link>
+    </div>
+  </section>;
+}
+
+export default PopularBikes
